e995b61cc071cab5727d84bc41722dd3754f738e
huangdongbiao <dongbiao.huang@zhenai.com>
2022-02-12 18:00:18 +0800

    first commit

---
+++ webhook.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const server = http.createServer((req, res) => {
+    console.log(req.method, req.url);
+    if (req.method == 'POST' && req.url == '/webhook') {
+        res.setHeader('Content-Type', 'application/json')
+        res.end(JSON.stringify({ ok: true }))
+    } else {
+        res.end('Not Found')
+    }
+})
+
+server.listen(4000, () => {
+    console.log('webhook服务运行在4000端口');
+})
