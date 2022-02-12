d3bccaff1175a25bd45455c816d37d59cc6f6034
huangdongbiao <dongbiao.huang@zhenai.com>
2022-02-12 18:55:56 +0800

    123

--- webhook.js
+++ webhook.js
@@ -1,7 +1,27 @@
 const http = require('http')
+const crypto = require('crypto')
+const SECRET = '123456'
+function sign(body) {
+    return `sha1=${crypto.createHmac('sha1', SECRET).update(body).digest('hex')}`
+}
+
 const server = http.createServer((req, res) => {
     console.log(req.method, req.url);
     if (req.method == 'POST' && req.url == '/webhook') {
+        let buffers = []
+        req.on('data', (buffer) => {
+            buffers.push(buffer)
+        })
+        req.on('end', () => {
+            let body = Buffer.concat(buffers)
+            let event = req.headers['x-gitHub-event']
+            // github请求过来的时候要传递body和一个signature，需要验证签名
+            let signature = req.headers['x-hub-signature']
+            if (signature !== sign(body)) {
+                console.log('伪造的请求，中断');
+                return res.end('Not ALLOW')
+            }
+        })
         res.setHeader('Content-Type', 'application/json')
         res.end(JSON.stringify({ ok: true }))
     } else {
