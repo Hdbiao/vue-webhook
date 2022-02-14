8704bac3257fcbe3dc088f85437eeebdbb4cb531
huangdongbiao <dongbiao.huang@zhenai.com>
2022-02-14 10:44:14 +0800

    msg

--- vue-back.sh
+++ vue-back.sh
@@ -7,11 +7,11 @@ git reset --hard origin/master
 git clean -f
 
 echo '拉取新代码'
-git pull origin/master
+git pull
 echo '构建代码'
 docker build -t vue-back:1.0 .
 echo '停止旧容器并删除旧容器'
 docker stop vue-back-container
-docker rm vue-back-container
+docker rm -f vue-back-container
 echo '启动新容器'
 docker run -d -p 3000:3000 --name vue-back-container vue-back:1.0
